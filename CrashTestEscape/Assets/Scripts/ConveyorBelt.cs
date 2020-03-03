14a87a1e64442b64d8b0755e6a20ecfd26fb341f
Scott Little <scott.little@loop.colum.edu>
2020-03-03 16:56:20 -0600

    Create crate prefabs and start work on conveyor belt object spawner script

--- CrashTestEscape/Assets/Scripts/ConveyorBelt.cs
+++ CrashTestEscape/Assets/Scripts/ConveyorBelt.cs
@@ -13,14 +13,22 @@ public class ConveyorBelt : MonoBehaviour
     /// </summary>
     [SerializeField, Tooltip("This is the BoxCollider2D that is used by the SurfaceEffector2D component. Default value == 1")]
     private float m_movementDelay = 1;
-    
+
+    [SerializeField, Tooltip("This is the list of objects that will be spawned onto the conveyor belt.")]
+    private GameObject[] m_spawnableObjectList;
+
+    [SerializeField, Tooltip("This is the transform of the object that marks the spawn location.")]
+    private Transform m_spawnerTransform;
+
     /// <summary>
     /// The object that actually applies the conveyor belt effect using a SurfaceEffector2D and a BoxCollider2D set to use effector.
     /// 
     /// *NOTE* - This value must be manually assigned in the editor since there are two colliders on this object.
     /// </summary>
     private GameObject m_conveyorMovementObject;
-    
+
+    private int m_lastObjectSpawned = 0;
+
     private float m_conveyerTimer;
 
     private bool m_conveyorBeltActive;
@@ -42,7 +50,31 @@ public class ConveyorBelt : MonoBehaviour
         if (Time.time > m_conveyerTimer)
         {
             m_conveyorMovementObject.SetActive(!m_conveyorMovementObject.gameObject.activeSelf);
+
+            SpawnNewConveyorObject();
+
             ResetConveyorDelay();
+        }
+    }
+
+    /// <summary>
+    /// Used to spawn a new object when the conveyor belt's SurfaceEffected Collider turns on.
+    /// </summary>
+    private void SpawnNewConveyorObject()
+    {
+        if (ConveyorBeltActive)
+        {
+            if (m_lastObjectSpawned == 0)
+            {
+                Instantiate(m_spawnableObjectList[1], m_spawnerTransform);
+                Instantiate(m_spawnableObjectList[1], m_spawnerTransform.position, Quaternion.identity);
+                m_lastObjectSpawned = 1;
+            }
+            else if (m_lastObjectSpawned == 1)
+            {
+                Instantiate(m_spawnableObjectList[0], m_spawnerTransform);
+                m_lastObjectSpawned = 0;
+            }
         }
     }
 
--- CrashTestEscape/Assets/Scripts/InanimateObjectOnConveyor.cs
+++ CrashTestEscape/Assets/Scripts/InanimateObjectOnConveyor.cs
@@ -9,10 +9,21 @@ public class InanimateObjectOnConveyor : MonoBehaviour
     [SerializeField, Tooltip("This is the conveyor belt that this object will be moved by.")]
     private ConveyorBelt m_assignedConveyor;
 
+    [SerializeField, Tooltip("How long after entering a KillZone will this object be destroyed?.")]
+    private float m_selfDestructTimer = 1.0f;
+
+    IEnumerator SelfDestruct()
+    {
+        yield return new WaitForSecondsRealtime(m_selfDestructTimer);
+        Destroy(this);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         m_inanimateRigidbody = GetComponent<Rigidbody2D>();
+        m_inanimateRigidbody.freezeRotation = true;
+        m_assignedConveyor = GameObject.FindObjectOfType<ConveyorBelt>().GetComponent<ConveyorBelt>();
     }
 
     // Update is called once per frame
@@ -23,6 +34,14 @@ public class InanimateObjectOnConveyor : MonoBehaviour
             Vector2 stoppingVelocity = m_inanimateRigidbody.velocity;
             stoppingVelocity = new Vector2(0.0f, m_inanimateRigidbody.velocity.y);
             m_inanimateRigidbody.velocity = stoppingVelocity;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "KillZone")
+        {
+            StartCoroutine("SelfDestruct");
         }
     }
 }
